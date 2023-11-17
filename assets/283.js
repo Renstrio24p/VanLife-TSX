99a1ca1ef3ee109935e0637e24f81106b72f7345
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-11-17 03:26:35 +0000

    deploy: c291afed482d3bde46db67df4e4f157078c8faac

---
+++ assets/283.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkstandalone_react_ts"] = self["webpackChunkstandalone_react_ts"] || []).push([[283],{
+
+/***/ 1283:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ NotFound)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);\n/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9655);\n\n\n\nfunction NotFound() {\n  return /* @__PURE__ */ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(\"div\", { className: \"vans-notfound\", children: [\n    /* @__PURE__ */ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"h1\", { children: \"Sorry, the page you were looking for wasn't exist.\" }),\n    /* @__PURE__ */ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react_router_dom__WEBPACK_IMPORTED_MODULE_1__/* .NavLink */ .OL, { to: \"/\", className: \"button\", children: \"Return to home\" })\n  ] });\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMTI4My5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUk7QUFKdUM7QUFFNUIsU0FBUyxXQUFXO0FBQ2pDLFNBQ0Usd0VBQUMsU0FBSSxXQUFVLGlCQUNiO0FBQUEsMkVBQUMsUUFBRyxnRUFBa0Q7QUFBQSxJQUN0RCx1RUFBQywrREFBTyxJQUFDLElBQUksS0FBSyxXQUFXLFVBQVUsNEJBRXZDO0FBQUEsS0FDRjtBQUVKIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc3RhbmRhbG9uZS1yZWFjdC10cy8uL3NyYy9jb21wb25lbnRzL3BhZ2VzL05vdEZvdW5kLnRzeD9mY2IxIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE5hdkxpbmsgfSBmcm9tIFwicmVhY3Qtcm91dGVyLWRvbVwiO1xuXG5leHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBOb3RGb3VuZCgpIHtcbiAgcmV0dXJuIChcbiAgICA8ZGl2IGNsYXNzTmFtZT1cInZhbnMtbm90Zm91bmRcIj5cbiAgICAgIDxoMT5Tb3JyeSwgdGhlIHBhZ2UgeW91IHdlcmUgbG9va2luZyBmb3Igd2Fzbid0IGV4aXN0LjwvaDE+XG4gICAgICA8TmF2TGluayB0bz17XCIvXCJ9IGNsYXNzTmFtZT17XCJidXR0b25cIn0+XG4gICAgICAgIFJldHVybiB0byBob21lXG4gICAgICA8L05hdkxpbms+XG4gICAgPC9kaXY+XG4gIClcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///1283\n");
+
+/***/ })
+
+}]);
